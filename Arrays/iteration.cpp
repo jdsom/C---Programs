5ebfb3eb3411d2089aa230ee36c8124ad5c12c42
jdsom <jordansommerville1@hotmail.co.uk>
2023-03-20 14:25:55 +0000

    for loop and for each loop.

--- Arrays/iteration.cpp
+++ Arrays/iteration.cpp
@@ -17,5 +17,22 @@ int main()
     {
         std::cout << students[i] << "\n";
     }
+
+    std::cout << '\n';
+    std::cout << "The following is a foreach loop rather than a for loop.\n";
+
+    int marks[] = {65, 72, 81, 94};
+    // For each loop
+    for (std::string student : students)
+    {
+        std::cout << student << '\n';
+    }
+
+    std::cout << '\n';
+
+    for (int mark : marks)
+    {
+        std::cout << mark << '\n';
+    }
     return 0;
 }
