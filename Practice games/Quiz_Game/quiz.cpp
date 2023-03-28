1b7e1b597ad3b2af4c24d660b504918e648b46aa
jdsom <jordansommerville1@hotmail.co.uk>
2023-03-28 22:10:38 +0100

    For loop using an array and 2D array for Q&A

--- Practice games/Quiz_Game/quiz.cpp
+++ Practice games/Quiz_Game/quiz.cpp
@@ -2,7 +2,30 @@
 
 int main()
 {
-    std::cout << "***** QUIZ GAME *****\n";
+    std::string questions[] = {"1. What year was C++ created?:",
+                               "2. Who invented C++?: ",
+                               "3. What is the predecessor of C++?: ",
+                               "4. Is the Earth flat?: "};
 
+    std::string options[][4] = {{"A. 1969,""B. 1975","C. 1985", "D. 1989"},
+                                {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerberg"},
+                                {"A. C", "B. C+", "C. C--", "D. B++"},
+                                {"A. Yes", "B. No", "C. Sometime", "D. What's Earth?"}};
+                                
+
+    char answerKey[] = {'C', 'B', 'A', 'B'};
+    int size = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score;
+
+    for (int i = 0; i < size; i++){
+        std::cout << "***************************\n";
+        std::cout << questions[i] << '\n';
+        std::cout << "***************************\n";
+
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++){
+            std::cout << options[i][j] << '\n';
+        }
+    }
     return 0;
 }
