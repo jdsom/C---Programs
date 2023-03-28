8b791e3c26e45165766f3e4d88539e59d83787cd
jdsom <jordansommerville1@hotmail.co.uk>
2023-03-28 22:56:52 +0100

    completed quiz

--- Practice games/Quiz_Game/quiz.cpp
+++ Practice games/Quiz_Game/quiz.cpp
@@ -7,7 +7,7 @@ int main()
                                "3. What is the predecessor of C++?: ",
                                "4. Is the Earth flat?: "};
 
-    std::string options[][4] = {{"A. 1969,""B. 1975","C. 1985", "D. 1989"},
+    std::string options[][4] = {{"A. 1969","B. 1975","C. 1985", "D. 1989"},
                                 {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerberg"},
                                 {"A. C", "B. C+", "C. C--", "D. B++"},
                                 {"A. Yes", "B. No", "C. Sometime", "D. What's Earth?"}};
@@ -16,7 +16,7 @@ int main()
     char answerKey[] = {'C', 'B', 'A', 'B'};
     int size = sizeof(questions)/sizeof(questions[0]);
     char guess;
-    int score;
+    int score = 0;
 
     for (int i = 0; i < size; i++){
         std::cout << "***************************\n";
@@ -26,6 +26,22 @@ int main()
         for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++){
             std::cout << options[i][j] << '\n';
         }
+
+        std::cin >> guess;
+        guess = toupper(guess);
+
+        if(guess == answerKey[i]){
+            std::cout << "Correct!\n";
+            score++;
+        }
+        else{
+            std::cout << "Wrong!\n";
+            std::cout << "Answer: " << answerKey[i] << '\n';
+        }
     }
+    std::cout << "Results:\n";
+    std::cout << "Correct Guesses: " << score << "\n";
+    std::cout << "# of Questions: " << size << '\n';
+    std::cout << "Score: " << (score/(double)size)*100 << "%";
     return 0;
 }
