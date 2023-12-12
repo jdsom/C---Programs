20e3a1b2feb6c53bde93b2b8ce5321dc328e158b
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-12 19:04:55 +0000

    sub string used in a if statement.

--- Strings/subString.cpp
+++ Strings/subString.cpp
@@ -2,9 +2,22 @@
 #include <string>
 
 int main(){
-
+    /*
     std::string s = "Hello World.";
     std::string subString = s.substr(6,5); //6 is the starting value, 5 is the range.
 
-    std::cout << "The substring value is: '" << subString << "'.";
+    std::cout << "The substring value is: '" << subString << "'.\n\n\n";
+    */
+
+    std::string s = "This is a Hello World string.";
+    std::string stringtofind = "Hello";
+    std::string::size_type found = s.find(stringtofind);
+
+    if (found != std::string::npos)         //if found is not equal to std::string::npos
+    {
+        std::cout << "Substring found at position: " << found;  //output the position the substring was found else print this.
+    }
+    else{
+        std::cout << "The substring is not found.";
+    }
 }
