a6715243025f602563dfaf7606f45039d148ce4b
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-12 19:14:55 +0000

    Automatic type Deduction

---
+++ Automatic Type Deduction/auto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    auto c = 123.234; // double
+    auto d = 12;        // int  
+    auto e = 'a';       // char
+
+    int x = 123;
+    auto& y = x; //reference is of int& type
+
+    const auto x = 123; // x is a constant of int type
+
+}
