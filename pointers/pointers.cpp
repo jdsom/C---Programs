66379fe5848ad91fba2a93078b3760c24f0dcee7
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-12 17:30:06 +0000

    Explaining pointers so it's easy to look back at.

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -2,8 +2,9 @@
 
 int main(){
 
-    char c = 'a';
-    char* p = &c;
+    char c = 'a'; // declared and initialized variable.
+    char* address = &c;  //* makes it a pointer, = &c means the pointer is pointing towards the address of variable c    
+    char p = *address;       //p is equal to the pointer pointing to the address of variable c
     
-    std::cout << "The value of the dereferneced pointer is: " << *p;
+    std::cout << "The value of variable named p: " << *address << "\n"; // *address is pointing to the value in the location of the address
 }
