267cf3cf87efa2b4732d02a059ba81e780228438
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-13 18:15:14 +0000

    final exercises for the specific chapter in the book

---
+++ Exercises/auto.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    auto c = 'a';
+    auto num = 123;
+    auto num1 = 123.1342;
+
+    std::cout << c << "\n" << num << "\n" << num1;
+}
--- Exercises/finding_character.cpp
+++ Exercises/finding_character.cpp
@@ -3,5 +3,14 @@
 
 int main(){
 
-    
+    std::string mainString = "Hello C++ World.";
+    std::string subString = "C++";
+    auto mysubstringfound = mainString.find(subString);
+
+    if (mysubstringfound != std::string::npos){
+
+        std::cout << "The substring of the main string is " << subString << "\n";
+    }else{
+        std::cout << "We were not able to find the substring.\n";
+    }
 }
