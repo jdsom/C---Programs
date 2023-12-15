146154dd04d2ade7862faa928f4a9daa8dd5f157
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-15 13:51:04 +0000

    constants in C++

---
+++ Constants/constants.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //chapter 17
+
+int main(){
+
+    const int n = 5; // A declared constant can't be assigned to, it can only be initialized.
+    int arr[n] = {10, 20 ,30 ,40 ,50};
+}
