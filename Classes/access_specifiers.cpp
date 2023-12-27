0e2444e06d4a9aaf1cfc5025145a41af4c20f8ed
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-27 20:38:07 +0000

    Example of access modifiers

--- Classes/access_specifiers.cpp
+++ Classes/access_specifiers.cpp
@@ -1,4 +1,6 @@
-class MyClass
+#include <iostream>
+
+class MyExampleClass
 {
 public:
 
@@ -12,7 +14,7 @@ private:
     // has private access level
 };
     // The default visibility/access specifier for a class is private if none of the access specifiers is present:
-class MyClass
+class MyExampleClass
 {
     // everything in here
     // has PRIVATE access BY DEFAULT
@@ -23,3 +25,23 @@ struct MyStruct
     // everything in here
     // has PUBLIC access BY DEFAULT
 };
+
+
+class MyClass
+{
+private:
+    int x;
+public:
+    void printx()
+    {
+        std::cout << "The value of x is:" << x; // x is accessible to printx() 
+        }
+};
+
+int main() {
+    MyClass o; // Create an object
+    o.x = 123; // Error, x has private access and is not accessible to
+                // object o
+    o.printx(); // printx() is accessible from object o
+
+}
