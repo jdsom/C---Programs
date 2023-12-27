b856dbf908d4271353e8b340a9388b1ef6e608ef
jdsom <jordansommerville1@hotmail.co.uk>
2023-12-27 17:19:18 +0000

    making changes to exercises.

--- pointers and references/exercises/auto_and_dynamic.cpp
+++ pointers and references/exercises/auto_and_dynamic.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main(){
-    int x = 123;
-    int* y = new int;
-    *y = 456;
+    int x = 123; // automatic memory allocation (stack)
+    int* y = new int; // dynamic memory allocation (heap)
+    *y = x;           // initializing the value
 
     std::cout << "\nThe automatic storage duration is: " << x << " and the dynamic storage duration is: " << *y << std::endl;
     
-    delete y;
+    delete y;           // deallocating the memory for the dynamic memory allocation to prevent memory leaks.
 
 }
