4edea2c8da434b8e35571103ff198de8587de03e
jdsomm <jordansommerville1@hotmail.co.uk>
2024-01-26 23:39:11 +0000

    Overloading both << and >>

--- Classes & Constructors/Overloading Operators/input_output.cpp
+++ Classes & Constructors/Overloading Operators/input_output.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 
 class Point{
- private:  
-    double m_x{};
-    double m_y{};
-    double m_z{};
 
-public:
-    Point(double x=0.0, double y=0.0, double z=0.0)
-        : m_x(x), m_y(y), m_z(z){}
+    private:
+        double m_x;
+        double m_y;
+        double m_z;
+        static int count;
 
-    friend std::ostream& operator<<(std::ostream& out, const Point& point);
+    public:
+
+        Point(double x=0.0, double y=0.0, double z=0.0)
+            :m_x{x}, m_y{y}, m_z{z}{
+                count++;
+            }
+
+        friend std::ostream& operator<<(std::ostream& out, const Point& point);
+        friend std::istream& operator>>(std::istream& in, Point& point);
+
+        static int getCount(){return count;}
+        
 };
 
-std::ostream& operator<< (std::ostream& out, const Point& point){   
-        out << "point(" << point.m_x << ", " << point.m_y << ", " << point.m_z << ")";
-
-        return out;
+std::ostream& operator<<(std::ostream& out, const Point& point){
+    out << "Point(" << point.m_x << ", " << point.m_y << ", " << point.m_z << ")";
+    return out;
+    }
+std::istream& operator>>(std::istream& in, Point& point){
+    in >> point.m_x;
+    in >> point.m_y;
+    in >> point.m_z;
+    
+    return in;
     }
 
-int main()
-{
-    Point point1{2.0, 3.5, 4.0};
-    Point point2{6.0, 7.5, 8.0};
+int Point::count = 0;
 
-    std::cout << point1 << '\n' << point2 << '\n';
+int main(){
 
-    return 0;
+    Point p1(1.0, 2.3, 421.01);
+    Point p2(33.0, 213.2313, 41231.0123);
+    Point p3;
+
+    std::cout << "Enter values for p3: ";
+    std::cin >> p3;
+
+    std::cout << p1 << std::endl << p2 << std::endl << p3 << std::endl;
+    std::cout << "Number of points: " << Point::getCount() << std::endl;
 }
