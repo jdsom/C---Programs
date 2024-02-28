097a5700939c614ec5d30b080bd984a27f4f53a8
jdsom <jordansommerville1@hotmail.co.uk>
2024-02-28 19:21:06 +0000

    overloading subscipt operator

--- Classes & Constructors/Overloading Operators/subscript_operator.cpp
+++ Classes & Constructors/Overloading Operators/subscript_operator.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 
-class IntList{
-    int m_list[10];
+class IntList {
+private:
+  int m_list[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // Initial state
 
-    public:
-        int& operator[] (int index){
-            return m_list[index];
-        }
+public:
+  // Universal operator[] (can be used for both const and non-const objects)
+  int& operator[](int index) {
+    if (index < 0 || index >= 10) {
+      throw std::out_of_range("Index out of bounds!");
+    }
+    return m_list[index];
+  }
 
+  // Access function for const objects (returns by value for efficiency)
+  const int at(int index) const {
+    if (index < 0 || index >= 10) {
+      throw std::out_of_range("Index out of bounds!");
+    }
+    return m_list[index];
+  }
 };
 
-/*
-// Can also be implemented outside the class definition
-int& IntList::operator[] (int index)
-{
-    return m_list[index];
-}
-*/
+int main() {
+  IntList list;
+  list[2] = 3;  // Assignment (modifying non-const object)
+  std::cout << list[2] << '\n';
 
-int main()
-{
-    IntList list;
-    list[2] = 3; // set a value
-    std::cout << list[2] << '\n'; // get a value
+  const IntList clist;
+  std::cout << clist.at(2) << '\n';  // Read-only access (using at for const objects)
 
-    return 0;
+  return 0;
 }
